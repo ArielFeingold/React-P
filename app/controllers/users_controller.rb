8916f8217addf44bdc1a759c8ebc1538adadd048
ArielFeingold <feingold.ariel@gmail.com>
2018-08-30 12:52:57 -0400

    user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: {status: 200, msg: 'User was created.'}
+    else
+      render json: {status: 400, msg: user.errors.messages}
+    end
+  end
+
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
